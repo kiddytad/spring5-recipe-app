c98d33aa9c9bcc6b3dd0566ae2debe6b2d634ee5
kiddytad <datnx38@gmail.com>
2021-08-22 10:07:34 +0700

    Add base

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -1,5 +1,6 @@
 package guru.springframework;
 
+import guru.springframework.domain.Ingredient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
 	}
 }
---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,26 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("recipes", recipeService.getRecipe());
+
+        return "index";
+    }
+}
